638b6d9d9426d908f9443f2bde93f7c6f5b1762d
NaFi619 <adruha000@gmail.com>
2026-07-06 01:00:09 +0600

    net

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/Assignment_6/',
-  plugins: [react(), tailwindcss()]
+  plugins: [react(), tailwindcss()], 
 })
